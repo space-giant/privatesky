24b1e600fec41d4f3d275707d6e155cfe48ae3de
daniel-visoiu <9691701+daniel-visoiu@users.noreply.github.com>
2019-05-21 22:39:15 +0300

    Added initial artchain token sample.

---
+++ libraries/artchain/index.js
@@ -0,0 +1,16 @@
+if (typeof $$ !== "undefined" && typeof $$.blockchain === "undefined") {
+  const pskDB = require("pskdb");
+  $$.blockchain = pskDB.startDB("./database");
+}
+
+const path = require("path");
+
+let prefix = "..";
+
+//temporary fix
+//cwd and point of reference is different when loaded in domain vs agent
+if (process.cwd().indexOf("engine") === -1) {
+  prefix = "code";
+}
+
+$$.loadLibrary("token", path.join(prefix, path.dirname(__filename), "token"));
